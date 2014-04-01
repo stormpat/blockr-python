5bfc74ccbb9dbe92567d7da1484a259dbf6f48a3
Patrik Storm <patrik.storm@verkkojulkaisut.fi>
2014-04-01 16:31:37 +0300

    Added multi search to class methods. (allows for chaining)

--- blockr/api.py
+++ blockr/api.py
@@ -9,7 +9,7 @@ import utils
 from service import ApiService
 
 class Api(ApiService):
-    """ The main API class. Calls the Block API with GET requests. """
+    """ The main API class. Calls the Block API with HTTP GET requests. """
     def __init__(self, currency, data='json'):
         ApiService.__init__(self, currency)
         self.data = data
@@ -30,33 +30,49 @@ class Api(ApiService):
     # Block API
     def block_info(self, block):
         """ Get info about a block. Multiple call options. """
+        # Allow for a list of blocks..
+        block = utils.request_type(block)
+
         res = r.get(self.url + self.block + str(block))
         return self.execute(res)
 
     def block_transaction(self, block):
-        """ Add docblocks todo """
+        """ Get information about block transactions. """
+        # Allow for a list of blocks..
+        block = utils.request_type(block)
+
         res = r.get(self.url + self.block_tx + str(block))
         return self.execute(res)
 
     def block_transaction_raw(self, block):
-        """ Add docblocks todo """
+        """  Get information about block transactions in bitcoind format.  """
+        # Allow for a list of blocks..
+        block = utils.request_type(block)
+
         res = r.get(self.url + self.block_raw + str(block))
         return self.execute(res)
 
     # Transaction API
     def transaction(self, transaction):
-        """ Add docblocks todo """
+        """ Get information about a transaction """
+        # Allow for a list of blocks..
+        transaction = utils.request_type(transaction)
+
         res = r.get(self.url + self.tx_info + str(transaction))
         return self.execute(res)
 
     def transaction_unconfirmed(self, transaction):
-        """ Add docblocks todo """
+        """ Get information about a unconfirmed transaction """
+        # Allow for a list of transactions.
+        transaction = utils.request_type(transaction)
+
         res = r.get(self.url + self.tx_unconf + str(transaction))
         return self.execute(res)
 
     # Address API
     def address(self, address, confirmations=0):
-        """ Add docblocks todo """
+        """ Get information about a specific address. """
+        # Allow for a list of addresses.
         address = utils.request_type(address)
 
         confs = {'confirmations': confirmations}
