cdcc4e8d7a94e5000239b6e3da1dc65c6b33b775
Patrik Storm <patrik.storm@verkkojulkaisut.fi>
2014-04-02 10:51:11 +0300

    Allowed multiple ways to pass in data to request transformer method.

--- blockr/utils.py
+++ blockr/utils.py
@@ -3,7 +3,10 @@
 def request_type(request):
     """ If the request is for multiple addresses, build a str with separator """
     if isinstance(request, list):
-        request = ",".join(request)
+        # If the list is ints convert to string.
+        to_string = ''.join(str(e) for e in request)
+        # Finally build one string of the list.
+        request = ",".join(to_string)
     return request
 
 # def have_confs(confirmations):
